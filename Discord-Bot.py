4ad8d6ef1715732d86f2e42d3c82c3ffb5ffdd24
Vincent Thomas <vincent.nathan.thomas@gmail.com>
2021-01-20 18:47:48 +0100

    added reaction Tab (1/2)

--- Discord-Bot.py
+++ Discord-Bot.py
@@ -3,12 +3,17 @@ import discord
 import json
 import requests
 from discord.ext import commands
-from discord.ext.commands import has_permissions
+from discord.ext.commands import has_permissions, MissingRole
+
 
 api_key = 'eb9d4b88480f4a672b237fb2b39fb156'
 #Client
 client = commands.Bot(command_prefix='--')
 client.remove_command('help')
+reaction_title = ''
+reactions = {}
+
+
 
 # -- + Command Code
 @client.command(name='version')
@@ -49,10 +54,65 @@ async def dm(context):
 @client.command(name = 'kick', pass_context = True)
 @has_permissions(administrator = True)
 async def kick(context, member: discord.Member):
-    
-    await member.kick()
-    await context.send('User ' + member.display_name + ' has been kicked')
+    try:
+        CommandInvokeError = ('Whops! Something went wrong... either you\'ve spellt it wrong or you tried to kick a Admin')
+        await member.kick()
+        await context.send('User ' + member.display_name + ' has been kicked')
+    except:
+        await context.send(CommandInvokeError)
 
+@client.command(name = 'ban', pass_context = True)
+@has_permissions(administrator = True)
+async def ban(context, member: discord.Member, *, reason = None):
+
+    await member.ban(reason=reason)
+    await context.send('User ' + member.display_name + ' has been banned')
+
+
+@client.command(name='reaction_create_post')
+async def reaction_create_post(context):
+
+    embed = discord.Embed(title='React Here for Role!', color = 0x05cffc)
+    embed.add_field(name='Set Title', value = '--reaction_set_title \'New Title\'', inline = False)
+    embed.add_field(name='Add Role', value = '--reaction_add_role @Role EMOJI_HERE', inline = False)
+    embed.add_field(name='Remove Role', value = '--reaction_remove_role @Role', inline = False)
+    
+    await context.send(embed=embed)
+    await context.message.delete()
+
+@client.command(name='reaction_set_title')
+async def reaction_set_title(context, new_title):
+
+    global reaction_title
+    reaction_title = new_title
+    await context.send('The title for the message is now `' + reaction_title + '`!')
+    await context.message.delete()
+
+
+@client.command(name='reaction_add_role')
+async def reaction_add_role(context, role: discord.Role, reaction):
+
+    if role != None:
+        reactions[role.name] = reaction
+        await context.send('Role `' + role.name + '` has been added with the emoji ' + reaction)
+        await context.message.delete()
+    else:
+        await context.send('Please try again')
+
+    print(reactions)
+
+
+@client.command(name='reaction_remove_role')
+async def reaction_remove_role(context, role: discord.Role):
+
+    if role.name in reactions:
+        del reactions[role.name]
+        await context.send('Role `' + role.name + '` has been deleted!')
+        await context.message.delete()
+    else:
+        await context.send('That role was not added!')
+
+    print(reactions) 
 
 
 @client.event
